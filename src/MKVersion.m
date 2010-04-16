32803c713217dc47f53f833e7874efd1aa1c035a
Mikkel Krautz <mikkel@krautz.dk>
2010-04-17 00:38:01 +0200

    Add MKVersion. Used for getting the Mumble protocol versions, etc.

---
+++ src/MKVersion.m
@@ -0,0 +1,54 @@
+/* Copyright (C) 2009-2010 Mikkel Krautz <mikkel@krautz.dk>
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ - Neither the name of the Mumble Developers nor the names of its
+ contributors may be used to endorse or promote products derived from this
+ software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <MumbleKit/MKVersion.h>
+
+@implementation MKVersion
+
+//
+// Get the current version of the Mumble protocol that MumbleKit supports
+//
++ (unsigned int) hexVersion {
+	return 0x10201;
+}
+
+//
+// MumbleKit's "release" string.  This is the release string that is sent to
+// the server.
+//
+// fixme(mkrautz): Does the server expect anything here, or are we free to use whatever?
+//                 We should probably not use this version string when sending to the Mumble
+//                 usage stats.
+//
++ (NSString *) releaseString {
+	return @"MumbleKit (1.2.0 compatible)";
+}
+
+@end
