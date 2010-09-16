a14d984fb01d08a1e05dbb4dd228e8baea83ac6b
Mikkel Krautz <mikkel@krautz.dk>
2010-09-16 21:32:47 +0200

    Add MKCertificate test utility to the tree.

---
+++ test/cert.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import <MumbleKit/MKCertificate.h>
+
+// This is just a temporary tool to test MKCertificate
+int main(int argc, char *argv[]) {
+	NSLog(@"Nothing to see, yet.");
+}
