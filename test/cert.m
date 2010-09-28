b6e358ec9886f209b87db467b8122057a4aad8af
Mikkel Krautz <mikkel@krautz.dk>
2010-09-28 18:51:47 +0200

    Update MKCertificate commandline utility to print the hexDigest of a certificate.

--- test/cert.m
+++ test/cert.m
@@ -3,5 +3,14 @@
 
 // This is just a temporary tool to test MKCertificate
 int main(int argc, char *argv[]) {
-	NSLog(@"Nothing to see, yet.");
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+	NSData *data = [NSData dataWithContentsOfFile:@"Certificates.cer"];
+	MKCertificate *cert = [MKCertificate certificateWithCertificate:data privateKey:nil];
+	if ([cert hasCertificate]) {
+		NSLog(@"digest = %@", [cert hexDigest]);
+	}
+
+	[pool release];
+	return 0;
 }
