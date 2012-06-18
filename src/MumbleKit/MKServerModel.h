802f161a8887270d7f3c9e70ff023620188f150d
Emilio Pavia <emilio.pavia@gmail.com>
2012-06-18 12:54:15 +0200

    Move MKPermission to its own header file

--- src/MumbleKit/MKServerModel.h
+++ src/MumbleKit/MKServerModel.h
@@ -6,28 +6,10 @@
 #import <MumbleKit/MKChannel.h>
 #import <MumbleKit/MKConnection.h>
 #import <MumbleKit/MKTextMessage.h>
+#import <MumbleKit/MKPermission.h>
 
 @class MulticastDelegate;
 @class MKServerModel;
-
-typedef enum {
-    MKPermissionNone             = 0x00000,
-    MKPermissionWrite            = 0x00001,
-    MKPermissionTraverse         = 0x00002,
-    MKPermissionEnter            = 0x00004,
-    MKPermissionSpeak            = 0x00008,
-    MKPermissionMuteDeafen       = 0x00010,
-    MKPermissionMove             = 0x00020,
-    MKPermissionMakeChannel      = 0x00040,
-    MKPermissionLinkChannel      = 0x00080,
-    MKPermissionWhisper          = 0x00100,
-    MKPermissionTextMessage      = 0x00200,
-    MKPermissionMakeTempChannel  = 0x00400,
-    MKPermissionKick             = 0x10000,
-    MKPermissionBan              = 0x20000,
-    MKPermissionRegister         = 0x40000,
-    MKPermissionSelfRegister     = 0x80000,
-} MKPermission;
 
 /**
  * MKServerModelDelegate is the delegate of MKServerModel.
